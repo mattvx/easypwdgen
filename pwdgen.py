b528b26ff9d9cdda63dcda547d2f649c524bc1fb
mattvx <93927786+mattvx@users.noreply.github.com>
2021-11-09 20:30:55 +0100

    Create pwdgen.py
    first version of python pwd gen.. ehehehhe

---
+++ pwdgen.py
@@ -0,0 +1,49 @@
+import random
+import string
+import sys
+
+helper = "\n\tCommand usage:\tpwdgen.py x charset\n\t\tWhere x is an integer defining password lenght\n\t\tAnd \"charset\" is available options:\n\n\t\t\tl = only lower\n\t\t\tu = only upper\n\t\t\tn = only numbers\n\t\t\ts = only symbols.\n\n\t\tThere are also combinations available:\n\n\t\t\tl,lu,lun,luns\n"
+
+try:
+    lenght = int(sys.argv[1])
+
+    low = string.ascii_lowercase
+    up = string.ascii_uppercase
+    num = string.digits
+    sym = string.punctuation
+
+    if sys.argv[2] == 'l':
+        all = low
+    if sys.argv[2] == 'u':
+        all = up
+    if sys.argv[2] == 'n':
+        all = num
+    if sys.argv[2] == 's':
+        all = sym    
+    if sys.argv[2] == 'lu':
+        all = low + up
+    if sys.argv[2] == 'lun':
+        all = low + up + num
+    if sys.argv[2] == 'luns':
+        all = low + up + num + sym
+
+    temp_pass = random.sample(all, lenght)
+
+    password = "".join(temp_pass)
+
+    print(password)  
+
+
+except ValueError:
+    print(helper)
+except IndexError:
+    print(helper)
+except NameError:
+    print(helper)
+except TypeError:
+    print(helper)
+
+
+
+
+
